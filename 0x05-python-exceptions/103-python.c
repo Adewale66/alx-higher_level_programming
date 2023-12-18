3b742fdabd1d6a7dcd8ae195f928ff8282894d0d
Adewale Kujore <adewalekujore6@gmail.com>
2023-12-18 13:07:29 +0100

    update

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * print_python_list - prints some basic info about Python lists
+ * @p: PyObject
+ * Return: void
+*/
+
+void print_python_list(PyObject *p)
+{
+}
+
+/**
+ * print_python_bytes - prints some basic info about Python bytes
+ * @p: PyObject
+ * Return: void
+*/
+
+void print_python_bytes(PyObject *p)
+{
+}
+
+/**
+ * print_python_float - prints some basic info about Python float
+ * @p: PyObject
+ * Return: void
+*/
+
+void print_python_float(PyObject *p)
+{
+}
