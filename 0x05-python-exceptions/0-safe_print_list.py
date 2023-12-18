647c9f24205f653d1928dd27a2928f884f2f6d6b
Adewale Kujore <adewalekujore6@gmail.com>
2023-12-18 12:39:24 +0100

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
     try:
-        for i in range(x):
+        for i in range(0, x):
             print(my_list[i], end="")
         print()
         return i + 1
     except IndexError:
-        print()
-        return i
-    except TypeError:
         print()
         return i
