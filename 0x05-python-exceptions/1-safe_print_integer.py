d1106bc95664841d71d7162d76af0f96a6412f99
Adewale Kujore <adewalekujore6@gmail.com>
2023-12-18 12:53:55 +0100

    update

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,8 @@
 
 def safe_print_integer(value):
     try:
+        value = int(value)
         print("{:d}".format(value))
         return True
-    except:
+    except ValueError:
         return False
