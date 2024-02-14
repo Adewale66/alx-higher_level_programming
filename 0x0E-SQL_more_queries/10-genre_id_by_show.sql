84b3f4dd7bb13f9552c26f6cc8c6a1cf5b65d7cd
Adewale Kujore <adewalekujore6@gmail.com>
2024-02-14 18:01:17 +0100

    task 11

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,4 +2,4 @@
 SELECT tv_shows.title, tsg.genre_id FROM tv_shows JOIN
 tv_show_genres tsg ON tv_shows.id = tsg.show_id JOIN
 tv_genres genres ON tsg.genre_id = genres.id
-ORDER BY tv_shows.title AND tsg.genre_id;
+ORDER BY tv_shows.title, tsg.genre_id;
