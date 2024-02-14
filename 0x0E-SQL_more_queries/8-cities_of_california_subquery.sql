3022cf1711a9e435f5f2ad138628c0073bce8868
Adewale Kujore <adewalekujore6@gmail.com>
2024-02-14 17:17:14 +0100

    task 9

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- Lists all the cities of California
-SELECT * FROM states WHERE id IN (SELECT state_id FROM cities WHERE state_id = 1 ORDER BY state_id ASC);
+SELECT * FROM states WHERE id IN (SELECT state_id FROM cities WHERE state_id = 1 ORDER BY id ASC);
