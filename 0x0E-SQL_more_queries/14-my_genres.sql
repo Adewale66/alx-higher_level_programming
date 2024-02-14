e5b8e2c11bb05860b0600ed561216656b37760ab
Adewale Kujore <adewalekujore6@gmail.com>
2024-02-14 18:28:03 +0100

    update

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,6 +2,6 @@
 SELECT name FROM tv_genres g
 LEFT JOIN tv_show_genres sg ON g.id = sg.genre_id
 LEFT JOIN tv_shows s ON sg.show_id = s.id
-WHERE s.title = 'Dexter';
+WHERE s.title = 'Dexter'
 GROUP BY name
 ORDER BY name;
