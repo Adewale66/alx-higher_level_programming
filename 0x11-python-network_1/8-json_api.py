09fc129011bcfff4338d8a50cfae3ea4e2d5d81d
Adewale Kujore <adewalekujore6@gmail.com>
2024-03-28 19:40:42 +0100

    task 9

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     else:
         q = ""
     url = "http://0.0.0.0:5000/search_user"
-    response = requests.post(url, params={"q": q})
+    response = requests.post(url, data={"q": q})
     try:
         json = response.json()
         if json:
