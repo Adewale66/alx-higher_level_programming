d0fff3cc1d29cce05bcde44163baf24b7ac8235b
Adewale Kujore <adewalekujore6@gmail.com>
2024-03-28 12:58:10 +0100

    update

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,8 +5,8 @@
 if __name__ == "__main__":
     import requests
 
-    url = 'https://intranet.hbtn.io/status'
+    url = 'https://alx-intranet.hbtn.io/status'
     response = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
-    print("\t- content: OK")
+    print("\t- content: {}".format(response.text))
