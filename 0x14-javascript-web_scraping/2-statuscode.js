2bd078b1abd1d15e112b762bc0d28b6ba5ac65e0
Adewale Kujore <Adewalekujore6@gmail.com>
2024-04-23 10:16:37 +0100

    done

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const request = require('request');
+request(process.argv[2], function (error, res, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`code : ${res.statusCode}`);
+  }
+});
